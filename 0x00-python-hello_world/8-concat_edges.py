d631dc9a0b6936cee838b07103d67bf06def8c25
Reinhardbett <bettkiptoo46@gmail.com>
2023-05-12 03:08:42 -0700

    Python Hello World

--- 0x00-python-hello_world/8-concat_edges.py
+++ 0x00-python-hello_world/8-concat_edges.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 str = "Python is an interpreted, interactive, object-oriented programming"
-str1 = str[39:]
-str = str1 + " " + "with Python"
+str1 = str[39:66]
+str = str1 + " " + "with" + " " + str[:6]
 print(str)
