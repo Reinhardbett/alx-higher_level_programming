14f3239c69b6e81afbb228bc6143e6a9debb7633
Reinhardbett <bettkiptoo46@gmail.com>
2023-06-04 10:49:10 -0700

    Define an empty class

---
+++ 0x06-python-classes/0-square.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+'''python3 -c 'print(__import__("0-square.py").__doc__)'
+Define a class Square
+'''
+
+
+class Square:
+    '''python3 -c 'print(__import__("0-square.py").Square.__doc__)'
+    Represent a square
+    '''
+    pass
