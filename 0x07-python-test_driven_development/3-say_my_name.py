26f2a76be8a6f0f2681965ab10447ead23ec7e70
Reinhardbett <bettkiptoo46@gmail.com>
2023-06-14 04:13:38 -0700

    Print concatenated string

---
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+'''The module returns a string with first name and last name
+'''
+
+
+def say_my_name(first_name, last_name=""):
+    '''print a string after checking if both inputs are strings
+    '''
+    if type(first_name) is not str:
+        raise TypeError("first_name must be a string")
+    if type(last_name) is not str:
+        raise TypeError("last_name must be a string")
+    print("My name is {} {}".format(first_name, last_name))
