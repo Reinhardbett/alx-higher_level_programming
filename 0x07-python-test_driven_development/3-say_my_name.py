559d10293fdb0bea479009ab88d980ab817373d2
Reinhardbett <bettkiptoo46@gmail.com>
2023-06-14 04:25:19 -0700

    Print names in a string

--- 0x07-python-test_driven_development/3-say_my_name.py
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -1,10 +1,15 @@
 #!/usr/bin/python3
-'''The module returns a string with first name and last name
+'''The module returns a name with first name and last name
 '''
 
 
 def say_my_name(first_name, last_name=""):
-    '''print a string after checking if both inputs are strings
+    '''Print a name after checking if both inputs are strings
+    Args:
+        first_name(str): The first name input
+        last_name(str): The last name input
+    Raises:
+        TypeError: if either inputs are not strings
     '''
     if type(first_name) is not str:
         raise TypeError("first_name must be a string")
