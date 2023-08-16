9e1b19ffa5a165885fc2d9a58475d3b2c1d5b54d
Reinhardbett <bettkiptoo46@gmail.com>
2023-08-16 11:31:58 -0700

    Display and order two columns

---
+++ 0x0D-SQL_introduction/10-top_score.sql
@@ -0,0 +1,5 @@
+-- List all records of a table ordered by the values of a specific column (score)
+-- Results should show only the score and name column (in this order)
+SELECT score, name
+FROM second_table
+ORDER BY score DESC;
