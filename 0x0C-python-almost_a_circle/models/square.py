d9d05051c40c7b77e02e0613361e8665dd02ad99
Reinhardbett <bettkiptoo46@gmail.com>
2024-01-06 23:36:56 -0800

    Add update method with args and kwargs

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -9,6 +9,7 @@ class Square(Rectangle):
     '''
     def __init__(self, size, x=0, y=0, id=None):
         '''Initialize the class properties
+        Assign size to width and height of superclass
         Args:
             1. size (int): square's single side
             2. x (int): x coordinate of the square
@@ -32,3 +33,29 @@ class Square(Rectangle):
         '''
         return ("[{}] ({}) {}/{} - {}".format(type(self).__name__,
                 self.id, self.x, self.y, self.width))
+
+    def update(self, *args, **kwargs):
+        '''Assign attributes from function calls
+        '''
+        if args or len(args) != 0:
+            count = 0
+            for arg in args:
+                if count == 0:
+                    self.id = arg
+                elif count == 1:
+                    self.size = arg
+                elif count == 2:
+                    self.x = arg
+                elif count == 3:
+                    self.y = arg
+                count += 1
+        elif kwargs or len(kwargs) != 0:
+            for k, v in kwargs.items():
+                if k == 'id':
+                    self.id = v
+                elif k == 'size':
+                    self.size = v
+                elif k == 'x':
+                    self.x = v
+                elif k == 'y':
+                    self.y = v
