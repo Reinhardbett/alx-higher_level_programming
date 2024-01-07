cd16b409bd15e673b94c031ab78b2f7b3acac738
Reinhardbett <bettkiptoo46@gmail.com>
2024-01-06 23:59:19 -0800

    Add to-dictionary method

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -135,3 +135,16 @@ class Rectangle(Base):
                     self.x = v
                 elif k == 'y':
                     self.y = v
+
+    def to_dictionary(self):
+        '''Return the dictionary representation
+        Must contain id, width, height, x, y
+        '''
+        dict = {
+                "id": self.id,
+                "width": self.width,
+                "height": self.height,
+                "x": self.x,
+                "y": self.y
+                }
+        return dict
