4cea743da107324022a28f511a77426618fc910d
Reinhardbett <bettkiptoo46@gmail.com>
2024-01-16 01:21:07 -0800

    Display table records from highest

---
+++ 0x0D-SQL_introduction/10-top_score.sql
@@ -0,0 +1,6 @@
+-- List all records of table second_table
+-- Display only score and name respectively
+-- Order records by score in descending manner
+SELECT score, name
+FROM `second_table`
+ORDER BY score DESC;
