3fd1fbff5ee1c7477982748084e2738ce5f50455
Reinhardbett <bettkiptoo46@gmail.com>
2024-01-18 10:34:59 -0800

    Extract pk-fk pairs without join

--- 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -2,7 +2,6 @@
 -- Use database 'hbtn_0d_usa'
 -- Order in ascending manner by id
 USE `hbtn_0d_usa`;
-SELECT id, name
-FROM `cities`
+SELECT `id`, name` FROM `cities`
 WHERE `state_id` = 1
-ORDER BY id ASC;
+ORDER BY `id` ASC;
