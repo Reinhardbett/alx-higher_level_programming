4b8f5fe74a81cc33d93797ddfc6bc0df76d309d3
Reinhardbett <bettkiptoo46@gmail.com>
2024-06-20 06:59:05 -0700

    fix: ensure all output is rounded off

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -4,5 +4,5 @@ const argv = require('process').argv;
 if (isNaN(Number(argv[2]))) {
   console.log('Not a number');
 } else {
-  console.log('My Number: ' + argv[2]);
+  console.log('My Number: ' + `${parseInt(argv[2])}`);
 }
