9239efd06e42e1765ee26b5c348363fb0562836a
Reinhardbett <bettkiptoo46@gmail.com>
2024-07-04 09:30:33 -0700

    feat: send variable using POST

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # send a POST request to URL and display response body
-curl -s -X POST "$1" -d "email=test@gmail.com" -d "subject=I wll always be here for PLD"
+curl -s -X POST "$1" -d "email=test@gmail.com" -d "subject=I will always be here for PLD"
