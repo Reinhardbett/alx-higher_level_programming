9c76a49593638b642c09063dfe0664440ca5f540
Reinhardbett <bettkiptoo46@gmail.com>
2024-07-05 13:20:08 -0700

    chore: format response body in json format

---
+++ 0x11-python-network_1/8-json_api.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+'''Take in letter, send POST request using letter as param
+'''
+import requests
+import sys
+
+if __name__ == "__main__":
+    '''Accept and verify letter input'''
+    if len(sys.argv) == 2:
+        q = sys.argv[1]
+    else:
+        q = ""
+    '''Send POST request with letter parameter'''
+    r = requests.post('http://0.0.0.0:5000/search_user', data={'q': q})
+    try:
+        r_json = r.json()
+        if r_json:
+            '''Assuming result is a dict'''
+            print("[{}] {}".format(r_json['id'], r_json['name']))
+        else:
+            print("No result")
+    except requests.exceptions.JSONDecodeError:
+        print("Not a valid JSON")
