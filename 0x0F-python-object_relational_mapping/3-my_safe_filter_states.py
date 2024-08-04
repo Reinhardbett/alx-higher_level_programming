0c0b60a1c0b1c950e7a0628da25c59126d363b25
Reinhardbett <bettkiptoo46@gmail.com>
2024-08-04 07:54:42 -0700

    feat: add input through parameterized token

---
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+'''This script lists all states in the hbtn_0e_0_usa starting with uppercase N
+It then takes in an arg and displays all values that match it
+For this script, measures will be taken to avoid SQL injections
+'''
+import MySQLdb
+import sys
+
+if __name__ == "__main__":
+    # Connect to the MySQL database
+    # Take username, password, and database name from stdout
+    db = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=sys.argv[1],
+        passwd=sys.argv[2],
+        db=sys.argv[3]
+    )
+
+    # Create working environment for our connection
+    cur = db.cursor()
+
+    # Display table states in ascending order by states.id
+    # Specify the state name from the SQL query
+    # Use parameterized queries %s
+    # Input data will be treated as data, not executable code
+    query = "SELECT * FROM states\
+        WHERE name = %s\
+        ORDER BY id ASC"
+    cur.execute(query, (sys.argv[4],))
+    rows = cur.fetchall()
+    for row in rows:
+        print(row)
+
+    # Close connection to database and working environment
+    cur.close()
+    db.close()
