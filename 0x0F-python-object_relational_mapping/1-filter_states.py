3473ed5b5daf5f3c5cfeb19990fc43e522ab4a5f
Reinhardbett <bettkiptoo46@gmail.com>
2024-08-04 03:24:51 -0700

    feat: specify the beginning letter of returned states

---
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+'''This script lists all states in the hbtn_0e_0_usa starting with uppercase N
+'''
+import MySQLdb
+import sys
+
+if __name__ == "__main__":
+    # Connect to the MySQL database
+    # Take username, password, and database name from stdout
+    db = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=sys.argv[1],
+        passwd=sys.argv[2],
+        db=sys.argv[3]
+    )
+
+    # Create working environment for our connection
+    cur = db.cursor()
+
+    # Display table states in ascending order by states.id
+    # Specify the beginning letter of each state
+    cur.execute(
+        "SELECT * FROM states\
+        WHERE name LIKE 'N%'\
+        ORDER BY id ASC"
+    )
+    rows = cur.fetchall()
+    for row in rows:
+        print(row)
+
+    # Close connection to database and working environment
+    cur.close()
+    db.close()
